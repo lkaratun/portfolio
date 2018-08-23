ef5a3224492cdeaf188003db20b80b2fbe355e6d
lkaratun <lev.karatun@gmail.com>
2018-08-23 13:41:47 -0400

    Nav links highlight on scroll

--- app.js
+++ app.js
@@ -1,51 +1,86 @@
-let navLinks = document.querySelectorAll(".scroll-link");
-navLinks.forEach(link => link.addEventListener("click", handleNavigationClick));
-let navArrow = document.querySelector("#downArrow");
-navArrow.addEventListener("click", clickArrow);
+const navLinks = document.querySelectorAll(".scroll-link");
+navLinks.forEach(link => {
+  link.addEventListener("click", handleNavigationClick);
+});
+
+const sectionsCoords = [];
+
+const sections = document.querySelectorAll("section");
+sections.forEach(section => {
+  sectionsCoords.push(section.offsetTop);
+});
+const sectionsBreakpoints = [];
+for (let i = 0; i < sectionsCoords.length - 1; i++) {
+  sectionsBreakpoints.push((sectionsCoords[i] + sectionsCoords[i + 1]) / 2);
+}
+console.log(sectionsCoords);
+console.log(sectionsBreakpoints);
 
 
-function clickArrow(e) {
-  restoreLinksAppearance(navLinks);
-  document.querySelector("#about-link").style.textDecoration = "underline";
-  let scrollTarget = document.querySelector(e.target.parentElement.getAttribute("href"));
-  console.log(scrollTarget);
+function handleScroll() {
+  for (let breakpointIndex = 0; breakpointIndex < sectionsBreakpoints.length; breakpointIndex++) {
+    if (window.scrollY < sectionsBreakpoints[breakpointIndex]) {
+      highlightLink(navLinks[breakpointIndex]);
+      return;
+    }
+  }
+  highlightLink(navLinks[sectionsBreakpoints.length]);
+}
+
+const navArrow = document.querySelector("#downArrow");
+navArrow.addEventListener("click", handleArrowClick);
+
+
+
+
+function handleArrowClick(e) {
+  highlightLink(document.querySelector("#about-link"));
+  const scrollTarget = document.querySelector("#about");
   smoothScroll(scrollTarget, 300);
 }
 
 function handleNavigationClick(e) {
-  restoreLinksAppearance(navLinks);
-  e.target.style.textDecoration = "underline";
-  let scrollTarget = document.querySelector(e.target.getAttribute("href"));
+  highlightLink(e.target);
+  const scrollTarget = document.querySelector(e.target.getAttribute("href"));
   smoothScroll(scrollTarget, 300);
 }
 
+function highlightLink(target) {
+  restoreLinksAppearance(navLinks);
+  target.style.textDecoration = "underline";
+}
 function restoreLinksAppearance(links) {
-  links.forEach(link => link.style.textDecoration = "none")
+  links.forEach(link => { link.style.textDecoration = "none"; });
 }
 
 
-let scrollThreshold = window.innerHeight / 4;
+
+
+const navBar = document.querySelector("#navbar");
+const navBarHeight = navBar.getBoundingClientRect().height;
+const scrollThreshold = window.innerHeight / 4;
+
 window.onscroll = () => {
+  setNavBarVisibility(navBar, scrollThreshold);
+  handleScroll();
+};
+function setNavBarVisibility(navBar, scrollThreshold) {
   if (window.pageYOffset > scrollThreshold) {
-    document.getElementById("navbar").style.top = "0";
-  } else {
-    document.getElementById("navbar").style.top = "-70px";
+    navBar.style.top = "0";
+  }
+  else {
+    navBar.style.top = `-${navBarHeight}px`;
   }
 }
 
-
-
-let navBarHeight = document.querySelector("#navbar").getBoundingClientRect().height;
-
 function smoothScroll(target, duration) {
-  // let targetPosition = target.getBoundingClientRect().top - navBarHeight;
-  let targetPosition = target.getBoundingClientRect().top;
-  let startPosition = window.pageYOffset;
+  const targetPosition = target.getBoundingClientRect().top;
+  const startPosition = window.pageYOffset;
   let startTime = null;
   function animation(currentTime) {
     if (startTime === null) { startTime = currentTime; }
-    let timeElapsed = currentTime - startTime;
-    let run = easeOutQuad(timeElapsed, startPosition, targetPosition, duration);
+    const timeElapsed = currentTime - startTime;
+    const run = easeOutQuad(timeElapsed, startPosition, targetPosition, duration);
     window.scrollTo(0, run);
     if (timeElapsed < duration) { requestAnimationFrame(animation); }
   }
@@ -57,10 +92,3 @@ function smoothScroll(target, duration) {
 
   requestAnimationFrame(animation);
 }
-
-
-// document.querySelectorAll('.scroll-link').forEach((link) => link.addEventListener('click', (e) => {
-//   // console.log(document.querySelector(e.target.getAttribute("href")));
-//   let target = document.querySelector(e.target.getAttribute("href"));
-//   smoothScroll(target, 300);
-// }));
