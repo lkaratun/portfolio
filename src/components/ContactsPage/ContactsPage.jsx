a67b9d372b172eb08b3b8fdcef9a3ee28c485f46
Lev Karatun <levkarat@amazon.com>
2021-05-30 12:21:48 -0700

    Create React svg components

--- src/components/ContactsPage/ContactsPage.jsx
+++ src/components/ContactsPage/ContactsPage.jsx
@@ -1,5 +1,7 @@
 import './ContactsPage.scss';
 import Email from '../../logos/Email';
+import Linkedin from '../../logos/Linkedin';
+import Github from '../../logos/Github';
 
 export default function ContactsPage() {
 	return (
@@ -7,33 +9,17 @@ export default function ContactsPage() {
 			<h4>Let's get in touch!</h4>
 			<div className="subHeader">
 				<a href="mailto:lev.karatun@gmail.com" target="_blank" rel="noreferrer">
-					{/* <object type="image/svg+xml" data="../../logos/email.svg" className="logo">
-						Email logo
-					</object> */}
 					<Email />
 					E-mail
 				</a>
-				{/* <a href="mailto:lev.karatun@gmail.com" target="_blank" rel="noreferrer">
-					{' '}
-					<object type="image/svg+xml" data="logos/email.svg" className="logo">
-						Email logo
-					</object>
-					E-mail
-				</a>
-				<a href="https://www.linkedin.com/in/lkaratun" target="_blank" rel="noreferrer">
-					{' '}
-					<object type="image/svg+xml" data="logos/linkedin.svg" className="logo">
-						LinkedIn logo
-					</object>
+				<a href="mailto:lev.karatun@gmail.com" target="_blank" rel="noreferrer">
+					<Linkedin />
 					LinkedIn
 				</a>
-				<a href="https://github.com/lkaratun" target="_blank" rel="noreferrer">
-					{' '}
-					<object type="image/svg+xml" data="logos/github.svg" className="logo">
-						GitHub logo
-					</object>
+				<a href="mailto:lev.karatun@gmail.com" target="_blank" rel="noreferrer">
+					<Github />
 					GitHub
-				</a> */}
+				</a>
 			</div>
 		</section>
 	);
