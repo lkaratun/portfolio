28918b04d7a5d8e278056daa0fa548dd9fae9084
Lev Karatun <levkarat@amazon.com>
2021-05-30 12:30:33 -0700

    Fix links styling

--- src/components/ContactsPage/ContactsPage.jsx
+++ src/components/ContactsPage/ContactsPage.jsx
@@ -8,15 +8,15 @@ export default function ContactsPage() {
 		<section id="contacts" className="page">
 			<h4>Let's get in touch!</h4>
 			<div className="subHeader">
-				<a href="mailto:lev.karatun@gmail.com" target="_blank" rel="noreferrer">
+				<a href="mailto:lev.karatun@gmail.com" target="_blank" rel="noopener noreferrer">
 					<Email />
 					E-mail
 				</a>
-				<a href="mailto:lev.karatun@gmail.com" target="_blank" rel="noreferrer">
+				<a href="https://www.linkedin.com/in/lkaratun" target="_blank" rel="noopener noreferrer">
 					<Linkedin />
 					LinkedIn
 				</a>
-				<a href="mailto:lev.karatun@gmail.com" target="_blank" rel="noreferrer">
+				<a href="https://github.com/lkaratun" target="_blank" rel="noopener noreferrer">
 					<Github />
 					GitHub
 				</a>
