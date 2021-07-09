25aa5247baa4269e646a464d624c513ce8e434f7
Lev Karatun <levkarat@amazon.com>
2021-07-08 23:25:07 -0700

    Project cards styling

--- src/components/ProjectsPage/ProjectCard/ProjectCard.jsx
+++ src/components/ProjectsPage/ProjectCard/ProjectCard.jsx
@@ -1,34 +1,27 @@
-import { useState } from 'react';
-import Enlarge from '../../../icons/Enlarge';
-import Shrink from '../../../icons/Shrink';
 import './ProjectCard.scss';
 
 export default function Project({ header, contents, screenshots, links }) {
 	const { hq, lq, alt } = screenshots;
 	const { result, code } = links;
 
-	const [expanded, setExpanded] = useState(false);
-
 	return (
 		<div className="cp-project-card">
-			<div className="screenshot-container" onClick={() => setExpanded(!expanded)}>
-				<img className="screenshot" src={lq} lq-image-url={lq} hq-image-url={hq} alt={alt} />
-				<Enlarge hidden={expanded} />
-				<Shrink hidden={!expanded} />
-			</div>
+			<img className="screenshot" src={lq} lq-image-url={lq} hq-image-url={hq} alt={alt} />
 
-			<h3>{header}</h3>
-			{/* <div className="project-description">{contents}</div> */}
-			{result && (
-				<a href={result} target="_blank" className="project-link" rel="noreferrer">
-					Visit site
-				</a>
-			)}
-			{code && (
-				<a href={result} target="_blank" className="project-link" rel="noreferrer">
-					View source code
-				</a>
-			)}
+			<div className="details">
+				<h3>{header}</h3>
+				{/* <div className="project-description">{contents}</div> */}
+				{result && (
+					<a href={result} target="_blank" className="project-link" rel="noreferrer">
+						Visit site
+					</a>
+				)}
+				{code && (
+					<a href={result} target="_blank" className="project-link" rel="noreferrer">
+						View source code
+					</a>
+				)}
+			</div>
 		</div>
 	);
 }
